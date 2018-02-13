b252f385bc7bb7d886129154a128b5c9b333f609
abelrodriguez <tecnico@abelrodriguez.es>
2018-02-13 20:44:49 +0100

    Installed webpack + typeorm + angular2 + SASS + Mysql connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,49 @@
+import 'zone.js/dist/zone-mix';
+import 'reflect-metadata';
+import 'polyfills';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+// NG Translate
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
 import { CoreModule } from './core/core.module';
 
+import { WebviewDirective } from 'app/directives/webview.directive';
 
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WebviewDirective
   ],
   imports: [
     BrowserModule,
-    CoreModule
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    CoreModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (HttpLoaderFactory),
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/model/index.ts
+++ src/app/model/index.ts
@@ -1,28 +1,28 @@
-import "reflect-metadata";
-import {createConnection} from "typeorm";
-import { Items } from "./entities/items.entity";
-
-createConnection().then(async connection => {
-
-    // console.log("Inserting a new user into the database...");
-    const item = new Items();
-    item.item_name = "Nombre";
-    await connection.manager.save(item);
-    console.log("Item saved: " + item.item_pk);
-    // const user = new User();
-    // user.firstName = "Timber";
-    // user.lastName = "Saw";
-    // user.age = 25;
-    // await connection.manager.save(user);
-    // console.log("Saved a new user with id: " + user.id);
-    //
-    // console.log("Loading users from the database...");
-    // const users = await connection.manager.find(User);
-    // console.log("Loaded users: ", users);
-
-    console.log("Succefully! You´re a crack");
-
-}).catch(error => console.log(error));
-
-// export * from './entities/items.entity'
-// export * from './entities/groups.entity'
+// import "reflect-metadata";
+// import {createConnection} from "typeorm";
+// import { Items } from "./entities/items.entity";
+//
+// createConnection().then(async connection => {
+//
+//     // console.log("Inserting a new user into the database...");
+//     // const item = new Items();
+//     // item.item_name = "Nombre";
+//     // await connection.manager.save(item);
+//     // console.log("Item saved: " + item.item_pk);
+//     // const user = new User();
+//     // user.firstName = "Timber";
+//     // user.lastName = "Saw";
+//     // user.age = 25;
+//     // await connection.manager.save(user);
+//     // console.log("Saved a new user with id: " + user.id);
+//     //
+//     // console.log("Loading users from the database...");
+//     // const users = await connection.manager.find(User);
+//     // console.log("Loaded users: ", users);
+//
+//     console.log("Succefully! You´re a crack");
+//
+// }).catch(error => console.log(error));
+//
+// // export * from './entities/items.entity'
+// // export * from './entities/groups.entity'
