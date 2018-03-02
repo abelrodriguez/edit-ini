0897df21f1650733217da766102273d1cb13b012
abelrodriguez <tecnico@abelrodriguez.es>
2018-03-02 18:17:38 +0100

    feat: Use CSS instead of SASS to have an easier start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { ConnectionbdService } from './core/connectionbd.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   constructor(public electronService: ElectronService,
